ba168a09e510f893b8b0d6410b775bdf4de7e754
lvjian <1349256352@qq.com>
2018-11-16 11:02:09 +0800

    输出补丁bug输出修改提交

--- src/com/whir/fxfk/test/testgit.java
+++ src/com/whir/fxfk/test/testgit.java
@@ -4,6 +4,7 @@ public class testgit {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("git-flow release v1.1.0");
+		System.out.println("git-flow hotfixs 修改补丁");
 	}
 
 }
